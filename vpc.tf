a0c41e04cd1872982b1ddca938cf686bdf700051
dan talpau <dan.talpau@yahoo.com>
2021-11-04 12:30:37 +0000

    gauravs network

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ module "network" {
     version = "3.4.0"
 
     project_id   = var.project
-    network_name = "gaurav-network"
+    network_name = "gaurav-network2"
     routing_mode = "GLOBAL"
 
     subnets = [
